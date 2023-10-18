7261370810813d28404451eea6cdd7fbce2640b4
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-10-19 01:12:33 +0500

    Update lab7task9.c

--- Labs/07/lab7task9.c
+++ Labs/07/lab7task9.c
@@ -1,3 +1,17 @@
+/*
+* Programmer: Ibrahim Johar Farooqi 
+* Date: 10 October 2023
+* Description: Junaid wants to keep track of all mobile phone bills in his X company branches. 
+               Let Y be the number of company mobile phones in each branch. 
+               Create a 2D array for bill amount, where keep track of branch ID in row subscript, 
+               mobile phone IDs in column subscript. Ask users to enter a bill for all mobile phones 
+               in all branches. Your program should print the following:
+                - Total bill for all branches
+                - Total bill for each branch
+                - Branch ID where maximum bill arrived
+                - Branch and Mobile Phone IDs where bill is highest of all mobile phones.
+*/
+
 #include <stdio.h>
 
 int main()
