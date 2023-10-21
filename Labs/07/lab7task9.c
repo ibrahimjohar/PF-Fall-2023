1759d233852d7e47ec1c3c38445e5ea8285f778f
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-10-21 13:00:43 +0500

    Update lab7task9.c

--- Labs/07/lab7task9.c
+++ Labs/07/lab7task9.c
@@ -16,16 +16,17 @@
 
 int main()
 {
-    int n, m;
-    int totalBranch = 0, maxBill = 0, maxBill_branch = 0, maxBill_phoneID = 0, maxBill_branchID = 0, billTotal = 0;
+    int n, m;//declare user input variables
+    int totalBranch = 0, maxBill = 0, maxBill_branch = 0, maxBill_phoneID = 0, maxBill_branchID = 0, billTotal = 0;//initialise variables to store specific information to be printed in the report as output
 
+    //input from user   
     printf("Enter the number of branches: ");
     scanf("%d", &n);
     printf("Enter the number of mobile phones in each branch: ");
     scanf("%d", &m);
 
-    int bills_arr[100][100];
-    for(int i=0; i<n; i++)
+    int bills_arr[100][100];//declare and initialise array 
+    for(int i=0; i<n; i++)//input from user bills
     {
         for (int j=0; j<m; j++)
         {
@@ -35,33 +36,34 @@ int main()
     }
     printf("\n");
 
-    for (int i=0; i<n; i++)
+    for (int i=0; i<n; i++)//access array to calculate report values
     {
-        totalBranch = 0;
-        for (int j=0; j<m; j++)
+        totalBranch = 0; //initialise branch total to 0 before calculating the next branch's total
+        for (int j=0; j<m; j++) 
         {
-            totalBranch = totalBranch + bills_arr[i][j];
+            totalBranch = totalBranch + bills_arr[i][j]; //calculation of branch total
 
-            if (bills_arr[i][j] > maxBill)
+            if (bills_arr[i][j] > maxBill) //check if at the index position the value is greater than maximum bill
             {
                 maxBill = bills_arr[i][j];
-                maxBill_branchID = i;
-                maxBill_phoneID = j;
+                maxBill_branchID = i; //store index in maximum bill branch ID variable
+                maxBill_phoneID = j; //store index in maximum bill phone ID variable
             }
         }
-        billTotal = billTotal + totalBranch;
+        billTotal = billTotal + totalBranch; //add branch total to the total bill variable
 
-        maxBill_branch = 0;
+        maxBill_branch = 0; //set branch index to zero
         
-        if (totalBranch > bills_arr[maxBill_branch][0])
+        if (totalBranch > bills_arr[maxBill_branch][0]) 
         {
             maxBill_branch = i;
         }
     }
 
+    //output the report
     printf("The total bill for all branches is: %d\n", billTotal);
     printf("\nThe total bill for each branch is: \n");
-    for(int i = 0; i<n; i++)
+    for(int i = 0; i<n; i++) //output each branch's total 
     {
         int totalBranch = 0;
         for(int j=0; j<m; j++)
