95da713bfa4b8917faf2d02a9488e29efb2b6bb0
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-10-24 23:43:43 +0500

    Update lab8task6.c

--- Labs/08/lab8task6.c
+++ Labs/08/lab8task6.c
@@ -1,7 +1,8 @@
 /*
  * Programmer: Ibrahim Johar Farooqi
  * Date: 24 October 2023
- * Description: You are given an array of integers. Write a C program that defines a user-defined function processArray to calculate the sum, maximum, and minimum values in the array.
+ * Description: You are given an array of integers. Write a C program that defines a user-defined function processArray to calculate 
+                the sum, maximum, and minimum values in the array.
                 The program should take the array and its size as parameters and use the function to compute these values.
                 Input: Array is [4, 8, 1, 15, 6]
                 Output:
@@ -12,36 +13,59 @@
 
 #include <stdio.h>
 
- processArray(int arr[], int size, int *sum, int *max, int *min){
-    *sum = 0;
-    *max = arr[0];
-    *min = arr[0];
-    
-    int i;
+void processArray(int arr[], int sizeArr, int *sum, int *maxVal, int *minVal);//function definition
 
-    for ( i = 0; i < size; ++i) {
-        *sum += arr[i];
+int main()
+{
+    int arr[100];//declare array
+    int sizeArr;//declare size of array
 
-        if (arr[i] > *max) {
-            *max = arr[i];
-        }
+    //input size of array from user
+    printf("Enter n: ");
+    scanf("%d", &sizeArr);
 
-        if (arr[i] < *min) {
-            *min = arr[i];
-        }
+    //input values of array from user
+    for (int i = 0; i < sizeArr; i++)
+    {
+        printf("Enter element %d: ", i);
+        scanf("%d", &arr[i]);
     }
-}
 
-int main() {
-    int arr[] = {4, 8, 1, 15, 6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int sum, max, min;
+    int sum, maxVal, minVal;//declare variables to store calculations 
 
-    processArray(arr, size, &sum, &max, &min);
-
-    printf("Array Sum:\n%d\n\n", sum);
-    printf("Maximum Value: %d\n", max);
-    printf("Minimum Value: %d\n", min);
+    processArray(arr, sizeArr, &sum, &maxVal, &minVal);//calling of the 'processArray' function
+    
+    //output the calculations and stats
+    printf("Array Sum: %d\n", sum);
+    printf("Maximum Value: %d\n", maxVal);
+    printf("Minimum Value: %d\n", minVal);
 
     return 0;
-}
+}//end main()
+
+void processArray(int arr[], int sizeArr, int *sum, int *maxVal, int *minVal)//function to calculate array sum, and determine max and min values
+{
+    //initialisation of variables
+    *sum = 0;
+    *maxVal = arr[0];
+    *minVal = arr[0];
+
+    for (int i = 0; i < sizeArr; i++)//for loop to calculate sum and determine max and min values
+    {
+        *sum = *sum + arr[i];
+
+        if (arr[i]>*maxVal)//check for max value
+        {
+            *maxVal = arr[i];
+        }
+        if (arr[i]<*minVal)//check for min value
+        {
+            *minVal = arr[i];
+        }
+    }//end for loop 
+
+}//end function 'processArray'
+
+
+
+
