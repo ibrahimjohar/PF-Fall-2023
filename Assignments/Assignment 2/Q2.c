281466d791d6cac49155441dfb3e60db8275d646
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-11-03 23:34:28 +0500

    Update Q2.c

--- Assignments/Assignment 2/Q2.c
+++ Assignments/Assignment 2/Q2.c
@@ -1,81 +1,92 @@
 /*
-Programmer: Ibrahim Johar Farooqi
-Date: 27 October 2023
-Description: Q2:
-			You are tasked with implementing a program to find the largest square submatrix of 1s in a given binary matrix. Write a C program that does the following:
-			1. Input a binary matrix (a 2D array of 0s and 1s) from the user. The matrix can be of any size.
-			2. Find and display the largest square submatrix of 1s in the input matrix.
-			3. Display the dimensions (rows and columns) of the largest square submatrix found.
-			4. If there are more than one largest square matrix, then find any one of them.
+*   Programmer: Ibrahim Johar Farooqi
+*   Date: 25 October 2023
+*   Description: Q2:
+                You are tasked with implementing a program to find the largest square submatrix of 1s in a given binary matrix.
+                Write a C program that does the following:
+                1. Input a binary matrix (a 2D array of 0s and 1s) from the user. The matrix can be of any size.
+                2. Find and display the largest square submatrix of 1s in the input matrix.
+                3. Display the dimensions (rows and columns) of the largest square submatrix found.
+                4. If there are more than one largest square matrix, then find any one of them.
 */
 
 #include <stdio.h>
 
 int main()
 {
-	int arr[100][100];
-	int r, c;
-	printf("Enter R (rows of matrix): ");
-	scanf("%d", &r);
-	printf("Enter C (columns of matrix): ");
-	scanf("%d", &c);
+    int r, c;
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+    int arr[100][100];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("Enter a binary number in the matrix element [%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    int maxsize = 0, size = 0, isFound = 0;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			printf("Enter binary element of matrix[%d][%d]", i, j);
-			scanf("%d", &arr[i][j]);
-		}
-	}
-	
-	int rCount = 0, cCount = 0;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			if (arr[i][j] == arr[i][j+1]) 
-		}
-	}
-	
-	/*
-	int rowCount = 0;
-	
-	int i = 0;
-	int rowFlag = 0;
-	for (i = 0; i < n;)
-	{
-		for (j = 0; j < m; j++)
-		{
-			if (arr[i][j] == arr[i][j+1])
-			{
-				rowCount++;
-				rowFlag = 1;
-			}
-			else
-			{
-				continue;
-			}
-		}
-		if (rowFlag == 0)
-		{
-			i++;
-		}
-	}
-	int colCount = 0;
-	int colFlag = 0;
-	int j = 0;
-	for (i = 0; i < rowCount; i++)
-	{
-		
-		if (arr[i][j] == arr[i+1][j])
-		{
-			colCount++;
-			colFlag = 1;
-		}
-		else
-		{
-			continue;
-		}
-	}*/
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (arr[i][j] == 1)
+            {
+                size = 1;
+                isFound = 1;
+
+                while (i + size < r && j + size < c && isFound)
+                {
+                    for (int l = i; l <= i + size; l++)
+                    {
+                        if (arr[l][j + size] == 0)
+                        {
+                            isFound = 0;
+                            break;
+                        }
+                    }
+                    for (int l = j; l <= j + size; l++)
+                    {
+                        if (arr[i + size][l] == 0)
+                        {
+                            isFound = 0;
+                            break;
+                        }
+                    }
+                    if (isFound == 1)
+                    {
+                        size++;
+                    }
+                }
+
+                if (size > maxsize) {
+                    maxsize = size;
+                }
+            }
+        }
+    }
+
+    if (maxsize == 0)
+    {
+        printf("no dimensions found!");
+    } 
+    else
+    {
+        printf("\nSubmatrix: \n");
+        for (int i = 0; i < maxsize; i++)
+        {
+            for (int j = 0; j < maxsize; j++)
+            {
+                printf("1  ");
+            }
+            printf("\n");
+        }
+        printf("\nThe largest submatrix of the matrix is of %d by %d", maxsize, maxsize);
+    }
+    return 0;
 }
