a89de8c70a41207ef2bbcf88cca4c4dc81b3d7d8
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-11-28 09:55:07 +0500

    Update lab11task2.c

--- Labs/11/lab11task2.c
+++ Labs/11/lab11task2.c
@@ -1,3 +1,12 @@
+/*
+*   Programmer: Ibrahim Johar Farooqi
+*   Date: 22 November 2023
+*   Description: An automobile company has a serial number for engine parts starting from AA0 to FF9. 
+                 The other characteristics of parts to be specified in a structure are: Year of manufacture, material and quantity manufactured.
+                 ● Specify a structure to store information corresponding to a part.
+                 ● Write a program to retrieve information on parts with serial numbers between BB1 and CC6.
+*/
+
 #include <stdio.h>
 
 // Define the structure for engine parts information
