752d2a6d3ea10c702f1ecffdc347039ded691ed5
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-12-07 16:20:39 +0500

    Update Q3.c

--- Assignments/A3/Q3.c
+++ Assignments/A3/Q3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-// Function to merge records from Personal.txt and Department.txt into Combine.txt
-int mergerecord();
+int mergerecord(); //function prototype 
 
-int main() {
-    // File pointers for Personal.txt, Department.txt, and Combine.txt
+int main()
+{
+    //file pointers declaration and initialisation
     FILE *personal = fopen("Personal.txt", "a+");
     FILE *department = fopen("Department.txt", "a+");
-    FILE *combine = fopen("Combine.txt", "a+");  // Open in append mode
+    FILE *combine = fopen("Combine.txt", "a+"); 
 
-    // Check if file opening was successful
+    //check for successful file opening
     if (personal == NULL || department == NULL || combine == NULL) {
         printf("Unable to open files.\n");
     } else {
-        // Call the mergerecord function
+        //calling mergerecord function
         mergerecord();
     }
 
-    // Close the file pointers
+    //closing files
     fclose(personal);
     fclose(department);
     fclose(combine);
@@ -25,96 +25,107 @@ int main() {
     return 0;
 }
 
-// Function to merge records from Personal.txt and Department.txt into Combine.txt
+//function definition to merge records from Personal.txt and Department.txt into Combine.txt
 int mergerecord()
 {
+    //declaring file pointers
     FILE *ptr1, *ptr2, *ptr3;
+    //declaring variables
     int id;
     int salary;
     char name[50];
 
-    // Open Personal.txt, Department.txt, and Combine.txt in append/update mode
+    //opening Personal.txt, Department.txt, and Combine.txt in "a+" mode
     ptr1 = fopen("Personal.txt", "a+");
     ptr2 = fopen("Department.txt", "a+");
-    ptr3 = fopen("Combine.txt", "a+");  // Open in append mode
+    ptr3 = fopen("Combine.txt", "a+"); 
 
-    // Check if file opening was successful
-    if (ptr1 == NULL || ptr2 == NULL || ptr3 == NULL) {
-        printf("Error opening one of the files\n");
+    //check for successful file opening
+    if (ptr1 == NULL || ptr2 == NULL || ptr3 == NULL)
+    {
+        printf("Error in opening one of the files\n");
         return 1;
     }
 
-    // Input loop for Personal.txt
+    //inputs for Personal.txt
     do {
         printf("Enter -1 to exit this input\n");
         printf("Enter Employee ID: ");
         scanf("%d", &id);
-        if (id != -1) {
-            // Write ID to Personal.txt
+        if (id != -1)
+        {
+            //writing ID to Personal.txt
             fprintf(ptr1, "%d\t", id);
 
             printf("Enter Employee name: ");
-            getchar();  // Consume the newline character left by scanf
+            getchar(); 
             fgets(name, sizeof(name), stdin);
-            // Write name to Personal.txt
+            //writing name to Personal.txt
             fprintf(ptr1, "%s", name);
         }
     } while (id != -1);
-    fclose(ptr1);
+    fclose(ptr1);//closing ptr1 file
 
-    // Input loop for Department.txt
+    //inputs for Department.txt
     do {
         printf("Enter -1 to exit this input\n");
         printf("Enter salary: ");
         scanf("%d", &salary);
-        if (salary != -1) {
+        if (salary != -1)
+        {
+            //writing Salary to Department.txt
             fprintf(ptr2, "%d\t", salary);
 
             printf("Enter ID: ");
             scanf("%d", &id);
+            //writing ID to Department.txt
             fprintf(ptr2, "%d\n", id);
         }
     } while (salary != -1);
-    fclose(ptr2);
+    fclose(ptr2);//closing ptr2 file
 
-    // Open Personal.txt, Department.txt, and Combine.txt in read mode
+    //opening Personal.txt, Department.txt, and Combine.txt in "r" mode
     ptr1 = fopen("Personal.txt", "r");
     ptr2 = fopen("Department.txt", "r");
-    ptr3 = fopen("Combine.txt", "a+");  // Open in append mode
+    ptr3 = fopen("Combine.txt", "a+");  
 
-    // Check if file opening was successful
-    if (ptr1 == NULL || ptr2 == NULL || ptr3 == NULL) {
-        printf("Error opening one of the files\n");
+    //check for successful file opening
+    if (ptr1 == NULL || ptr2 == NULL || ptr3 == NULL)
+    {
+        printf("Error in opening one of the files\n");
         return 1;
     }
 
-    // Merge records from Personal.txt and Department.txt into Combine.txt
-    while (fscanf(ptr1, "%d %[^\n]", &id, name) != EOF && fscanf(ptr2, "%d %d", &salary, &id) != EOF) {
-        // Write merged record to Combine.txt
+    //merging records from Personal.txt and Department.txt into Combine.txt
+    while (fscanf(ptr1, "%d %[^\n]", &id, name) != EOF && fscanf(ptr2, "%d %d", &salary, &id) != EOF)
+    {
+        //writing the merged record to Combine.txt
         fprintf(ptr3, "%d, %s, %d\n", id, name, salary);
     }
 
-    // Close the file pointers
+    //closing files
     fclose(ptr1);
     fclose(ptr2);
     fclose(ptr3);
 
-    // Open Combine.txt in read mode
+    //opening Combine.txt file for reading
     ptr3 = fopen("Combine.txt", "r");
 
-    // Check if file opening was successful
-    if (ptr3 == NULL) {
+    //check for successful file opening
+    if (ptr3 == NULL)
+    {
         printf("File could not be opened.");
         return 1;
     }
 
-    // Print the contents of Combine.txt
+    //printing Combine.txt
     char c;
-    while ((c = fgetc(ptr3)) != EOF) {
+    while ((c = fgetc(ptr3)) != EOF)
+    {
         putchar(c);
     }
 
-    // Close the file pointer
+    //closing ptr3 file
     fclose(ptr3);
     return 0;
 }
