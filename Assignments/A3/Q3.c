1ba38c6e15900a00f1917df2778cef7c712f7837
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-12-08 19:55:48 +0500

    Update Q3.c

--- Assignments/A3/Q3.c
+++ Assignments/A3/Q3.c
@@ -2,7 +2,7 @@
 *   Programmer: Ibrahim Johar Farooqi
 *   Date: 5 December 2023
 *   Description:
-                Question 2:
+                Question 3:
                 Create three text files named Department.txt, Personal.txt and Combine.txt. The Personal.txt file contains ID and Name. 
                 Whereas the Department.txt file contains ID and Salary. 
                 Write a function that takes input as record IDs and mergesthe details from both personal and department files and then add this entry into the Combine.txt file (ID, Name, Salary).
