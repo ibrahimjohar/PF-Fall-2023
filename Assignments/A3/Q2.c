b56e575af9daba286d617c6650e441425f001d36
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2023-12-08 19:52:39 +0500

    Update Q2.c

--- Assignments/A3/Q2.c
+++ Assignments/A3/Q2.c
@@ -39,6 +39,7 @@ void shuffleorder(int array[], int size);
 
 int main()
 {
+    printf("Ibrahim Johar Farooqi (23K-0074)\n");
     //declaration of departments arrays within the employee structure
     struct Employee hr[5];
     struct Employee finance[5];
